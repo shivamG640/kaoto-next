91e2e49a96d1269c97829c14f0b01a4089c12930
shivamgu <shivamgu@redhat.com>
2024-02-27 14:13:56 +0530

    feat(canvas): test commit

--- packages/ui/src/models/visualization/flows/kamelet-visual-entity.ts
+++ packages/ui/src/models/visualization/flows/kamelet-visual-entity.ts
@@ -1,5 +1,5 @@
 import { getCamelRandomId } from '../../../camel-utils/camel-random-id';
-import { ROOT_PATH } from '../../../utils';
+import { ROOT_PATH, setValue } from '../../../utils';
 import { EntityType } from '../../camel/entities';
 import { IKameletDefinition, IKameletMetadata, IKameletSpec } from '../../kamelets-catalog';
 import { KaotoSchemaDefinition } from '../../kaoto-schema';
@@ -14,7 +14,7 @@ export class KameletVisualEntity extends AbstractCamelVisualEntity {
   spec: IKameletSpec;
   metadata: IKameletMetadata;
 
-  constructor(kamelet: IKameletDefinition) {
+  constructor(public kamelet: IKameletDefinition) {
     super({ id: kamelet.metadata?.name, from: kamelet?.spec.template.from });
     this.id = (kamelet?.metadata?.name as string) ?? getCamelRandomId('kamelet');
     this.metadata = kamelet?.metadata ?? { name: this.id };
@@ -29,15 +29,107 @@ export class KameletVisualEntity extends AbstractCamelVisualEntity {
   }
 
   getComponentSchema(path?: string | undefined): VisualComponentSchema | undefined {
+    const customSchema = {
+      name: this.metadata.name,
+      title: this.spec.definition.title,
+      description: this.spec.definition.description,
+      type: this.metadata.labels['camel.apache.org/kamelet.type'],
+      icon: this.metadata.annotations['camel.apache.org/kamelet.icon'],
+      supportLevel: this.metadata.annotations['camel.apache.org/kamelet.support.level'],
+      catalogVersion: this.metadata.annotations['camel.apache.org/catalog.version'],
+      provider: this.metadata.annotations['camel.apache.org/provider'],
+      group: this.metadata.annotations['camel.apache.org/kamelet.group'],
+      namespace: this.metadata.annotations['camel.apache.org/kamelet.namespace'],
+      labels: {},
+      annotations: {}
+    }
+
     if (path === ROOT_PATH) {
       return {
         title: 'Kamelet',
         schema: this.getRootKameletSchema(),
-        definition: this.route,
+        definition: customSchema,
       };
     }
 
     return super.getComponentSchema(path);
+  }
+
+  updateModel(path: string | undefined, value: Record<string, unknown>): void {
+    if (!path) return;
+
+    if (path === ROOT_PATH) {
+      this.metadata.name = value.name as string;
+      this.spec.definition.title = value.title as string;
+      this.metadata.labels['camel.apache.org/kamelet.type'] = value.type as string;
+      this.metadata.annotations['camel.apache.org/kamelet.icon'] = value.icon as string;
+      this.metadata.annotations['camel.apache.org/kamelet.support.level'] = value.supportLevel as string;
+      this.metadata.annotations['camel.apache.org/catalog.version'] = value.catalogVersion as string;
+      this.metadata.annotations['camel.apache.org/provider'] = value.provider as string;
+      this.metadata.annotations['camel.apache.org/kamelet.group'] = value.group as string;
+      this.metadata.annotations['camel.apache.org/kamelet.namespace'] = value.namespace as string;
+
+      // I tried doing this first
+      // setValue(this.kamelet, path, this.kamelet);
+      // return;
+
+      // this for just for testing
+      const customKamelet = {
+        "apiVersion": "camel.apache.org/v1",
+        "kind": "Kamelet",
+        "metadata": {
+          "annotations": {
+              "camel.apache.org/catalog.version": "Shivam",
+              "camel.apache.org/kamelet.group": "Shivam",
+              "camel.apache.org/kamelet.icon": "Shivam",
+              "camel.apache.org/kamelet.support.level": "Shivam",
+              "camel.apache.org/provider": "Shivam"
+          },
+          "labels": {
+              "camel.apache.org/kamelet.type": "source"
+          },
+          "name": "Shivam"
+        },
+        "spec": {
+          "definition": {
+              "description": "Shivam",
+              "properties": {
+                  "period": {
+                      "default": 5000,
+                      "description": "Shivam",
+                      "title": "Period",
+                      "type": "integer"
+                  }
+              },
+              "title": "kamelet-4437",
+              "type": "object"
+          },
+          "template": {
+              "from": {
+                  "steps": [
+                      {
+                          "to": "https://random-data-api.com/api/v2/users"
+                      },
+                      {
+                          "to": "kamelet:sink"
+                      }
+                  ],
+                  "id": "from-2033",
+                  "parameters": {
+                      "period": "{{period}}",
+                      "timerName": "user"
+                  },
+                  "uri": "timer"
+              }
+          }
+        }
+      }
+
+      setValue(this.kamelet, path, customKamelet);
+      return;
+    }
+
+    super.updateModel(path, value);
   }
 
   private getRootKameletSchema(): KaotoSchemaDefinition['schema'] {
