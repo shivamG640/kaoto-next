2d76fca3cf33c49e64159e9300ad42c18d16d57d
Shivam Gupta <shvmgpt640@gmail.com>
2024-06-10 17:42:12 +0530

    fix(991): EIPs advanced properties grouping

--- packages/ui/src/components/Form/CustomAutoFields.tsx
+++ packages/ui/src/components/Form/CustomAutoFields.tsx
@@ -2,6 +2,10 @@ import { AutoField } from '@kaoto-next/uniforms-patternfly';
 import { ComponentType, createElement } from 'react';
 import { useForm } from 'uniforms';
 import { KaotoSchemaDefinition } from '../../models';
+import { getValue } from '../../utils';
+import { Card, CardBody, ExpandableSection, capitalize } from '@patternfly/react-core';
+import { useMemo } from 'react';
+import './CustomAutoFields.scss';
 
 export type AutoFieldsProps = {
   autoField?: ComponentType<{ name: string }>;
@@ -25,11 +29,48 @@ export function CustomAutoFields({
     return createElement(element, props, [createElement(autoField!, { key: '', name: '' })]);
   }
 
+  const actualFields = (fields ?? schema.getSubfields()).filter((field) => !omitFields!.includes(field));
+
+  const propertiesArray = useMemo(() => {
+    return actualFields.reduce(
+      (acc, name) => {
+        const group: string = getValue(schema.getField(name), 'group', '');
+        if (group === '' || group === 'common') {
+          acc.common.push(name);
+        } else {
+          acc.groups[group] ??= [];
+          acc.groups[group].push(name);
+        }
+        return acc;
+      },
+      { common: [], groups: {} } as { common: string[]; groups: Record<string, string[]> },
+    );
+  }, [actualFields]);
+
   return createElement(
     element,
     props,
-    (fields ?? schema.getSubfields())
-      .filter((field) => !omitFields!.includes(field))
-      .map((field) => createElement(autoField!, { key: field, name: field })),
+    <>
+      {propertiesArray.common.map((field) => (
+        <AutoField key={field} name={field} />
+      ))}
+
+      {Object.entries(propertiesArray.groups).map(([groupName, groupFields]) => (
+        <ExpandableSection
+          key={`${groupName}-section-toggle`}
+          toggleText={capitalize(`${groupName} properties`)}
+          toggleId="expandable-section-toggle"
+          contentId="expandable-section-content"
+        >
+          <Card className="nest-field-card">
+            <CardBody className="nest-field-card-body">
+              {groupFields.map((field) => (
+                <AutoField key={field} name={field} />
+              ))}
+            </CardBody>
+          </Card>
+        </ExpandableSection>
+      ))}
+    </>,
   );
 }
