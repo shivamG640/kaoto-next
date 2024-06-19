d4ce3b068e5f91477ab44f2ae70007ee2071aa88
Shivam Gupta <shvmgpt640@gmail.com>
2024-06-19 17:18:26 +0530

    feat(form): Filter parameters in the Canvas Form

---
+++ packages/ui/src/components/Form/customField/CustomExpandableSection.tsx
@@ -0,0 +1,32 @@
+import { FunctionComponent, PropsWithChildren, useEffect, useState } from 'react';
+import { ExpandableSection, capitalize } from '@patternfly/react-core';
+
+interface CustomExpandableSectionProps extends PropsWithChildren {
+  groupName: string;
+  isGroupExpanded: boolean;
+}
+
+export const CustomExpandableSection: FunctionComponent<CustomExpandableSectionProps> = (props) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  useEffect(() => {
+    setIsExpanded(props.isGroupExpanded);
+  }, [props.isGroupExpanded]);
+
+  const onToggle = (_event: React.MouseEvent, isExpanded: boolean) => {
+    setIsExpanded(isExpanded);
+  };
+
+  return (
+    <ExpandableSection
+      key={`${props.groupName}-section-toggle`}
+      toggleText={capitalize(`${props.groupName} properties`)}
+      toggleId="expandable-section-toggle"
+      contentId="expandable-section-content"
+      onToggle={onToggle}
+      isExpanded={isExpanded}
+    >
+      {props.children}
+    </ExpandableSection>
+  );
+};
