31a874240e0dbf35a9f8ea9a9acce2e3b3a248ac
Shivam Gupta <shvmgpt640@gmail.com>
2024-08-27 15:05:44 +0530

    Fix(catalog): Added pagination to the catalog modal

--- packages/ui/src/components/Catalog/BaseCatalog.tsx
+++ packages/ui/src/components/Catalog/BaseCatalog.tsx
@@ -1,5 +1,5 @@
-import { DataList, Gallery, Title } from '@patternfly/react-core';
-import { FunctionComponent, useCallback, useEffect, useRef } from 'react';
+import { DataList, Gallery, Pagination } from '@patternfly/react-core';
+import { FunctionComponent, useCallback, useState } from 'react';
 import './BaseCatalog.scss';
 import { CatalogLayout, ITile } from './Catalog.models';
 import { CatalogDataListItem } from './DataListItem';
@@ -14,7 +14,10 @@ interface BaseCatalogProps {
 }
 
 export const BaseCatalog: FunctionComponent<BaseCatalogProps> = (props) => {
-  const catalogBodyRef = useRef<HTMLDivElement>(null);
+  const itemCount = props.tiles?.length;
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(20);
+
   const onTileClick = useCallback(
     (tile: ITile) => {
       props.onTileClick?.(tile);
@@ -30,26 +33,43 @@ export const BaseCatalog: FunctionComponent<BaseCatalogProps> = (props) => {
     [onTileClick, props.tiles],
   );
 
-  useEffect(() => {
-    catalogBodyRef.current!.scrollTop = 0;
-  }, [props.tiles]);
+  const onSetPage = (_event: React.MouseEvent | React.KeyboardEvent | MouseEvent, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const onPerPageSelect = (
+    _event: React.MouseEvent | React.KeyboardEvent | MouseEvent,
+    newPerPage: number,
+    newPage: number,
+  ) => {
+    setPerPage(newPerPage);
+    setPage(newPage);
+  };
+
+  const paginatedCards = props.tiles?.slice((page - 1) * perPage, page * perPage);
 
   return (
     <>
-      <Title headingLevel="h2" size="xl">
-        Showing {props.tiles?.length} elements
-      </Title>
-      <div id="catalog-list" className="catalog-list" ref={catalogBodyRef}>
+      <Pagination
+        itemCount={itemCount}
+        perPage={perPage}
+        page={page}
+        onSetPage={onSetPage}
+        widgetId="top-example"
+        onPerPageSelect={onPerPageSelect}
+        ouiaId="PaginationTop"
+      />
+      <div id="catalog-list" className="catalog-list">
         {props.catalogLayout == CatalogLayout.List && (
           <DataList aria-label="Catalog list" onSelectDataListItem={onSelectDataListItem} isCompact>
-            {props.tiles?.map((tile) => (
+            {paginatedCards.map((tile) => (
               <CatalogDataListItem key={`${tile.name}-${tile.type}`} tile={tile} onTagClick={props.onTagClick} />
             ))}
           </DataList>
         )}
         {props.catalogLayout == CatalogLayout.Gallery && (
           <Gallery hasGutter>
-            {props.tiles?.map((tile) => (
+            {paginatedCards.map((tile) => (
               <Tile key={`${tile.name}-${tile.type}`} tile={tile} onClick={onTileClick} onTagClick={props.onTagClick} />
             ))}
           </Gallery>
